2c57811f29b8391446d1aa8804cc731c7115e328
AnupD12 <anupdambal@gmail.com>
2022-10-02 19:39:02 +0000

    Added Board Class, able to print the board without any ships or guess to the screen

--- run.py
+++ run.py
@@ -1,3 +1,64 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+
+class Board:
+    """
+    
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print("".join(row)) 
+
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "X" 
+
+        if (x, y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+
+        else:
+            return "Miss"
+
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannont add any more ships!")
+            else:
+                self.ships.append((x, y))
+                if self.type == "player":
+                    self.board[x][y] = "@"
+
+
+def random_point(size):
+    """
+    """
+    return randint(0, size - 1)
+
+
+def new_game():
+    """
+    
+    """
+
+    size = 5
+    num_ships = 4
+    print("-"*15)
+    print(f"Board size: {size}. Number of ships: {num_ships}")
+    # computer_game = Board(size, num_ships, "computer", type="computer")
+    # computer_game.print()
+
+new_game()
+
+
