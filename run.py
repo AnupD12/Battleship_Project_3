041adccf06f3b47a344b1b258de494326f9e6adb
AnupD12 <anupdambal@gmail.com>
2022-10-16 20:19:06 +0000

    Added in the abilty for the user to choose the size of the board and the number of ships

--- run.py
+++ run.py
@@ -34,30 +34,45 @@ class Board:
 
     def add_ship(self, x, y, type="computer"):
         if len(self.ships) >= self.num_ships:
-            print("Error: you cannont add any more ships!")
-            else:
-                self.ships.append((x, y))
-                if self.type == "player":
-                    self.board[x][y] = "@"
-
+            print("Error: you cannot add any more ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
+        print(self.ships)
 
 def random_point(size):
     """
     """
+    print(randint(0, size - 1))
     return randint(0, size - 1)
 
+
+def populate_board(size):
+    """
+    """
+    point_x = random_point(size)
+    point_y = random_point(size)
 
 def new_game():
     """
     
     """
 
+    # size = int(input("No. of row and Columns = "))
     size = 5
-    num_ships = 4
-    print("-"*15)
+    # num_ships = int(input("no. of ships = "))
+    num_ships = 2 
+    print("-"*15) 
     print(f"Board size: {size}. Number of ships: {num_ships}")
-    # computer_game = Board(size, num_ships, "computer", type="computer")
-    # computer_game.print()
+    computer_game = Board(size, num_ships, "computer", type="computer")
+    computer_game.print()
+    # point_x = random_point(size)
+    # point_y = random_point(size)
+    # new_ship = computer_game.add_ship(point_x, point_y)  
+    populate_board(size)
+    
+    
 
 new_game()
 
